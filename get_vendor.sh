48fa7012291a2b92ee3a50cbce939e508357c13e
Wang Xuerui <idontknw.wang@gmail.com>
2015-08-25 04:50:15 +0800

    bluetooth

--- get_vendor.sh
+++ get_vendor.sh
@@ -57,10 +57,20 @@ RIL="/lib/mtk-ril.so /lib/librilmtk.so /lib/libaed.so \
 AUDIO="/lib/hw/audio.primary.mt6595.so /lib/libblisrc.so /lib/libspeech_enh_lib.so /lib/libaudiocustparam.so /lib/libaudiosetting.so \
 /lib/libaudiocompensationfilter.so /lib/libcvsd_mtk.so /lib/libmsbc_mtk.so /lib/libaudiocomponentengine.so \
 /lib/libblisrc32.so /lib/libbessound_hd_mtk.so /lib/libmtklimiter.so /lib/libmtkshifter.so /lib/libaudiodcrflt.so \
-/lib/libtfa9890_interface.so /lib/libtinyxml.so \
-/lib/libbluetoothdrv.so /lib/libbluetooth_mtk.so"
+/lib/libtfa9890_interface.so /lib/libtinyxml.so"
+# bluetooth bits moved to individual section
 
-SYSTEM="$FIRMWARE $WIFI $GL $DRM $RIL $AUDIO"
+BLUETOOTH="/bin/mtkbt \
+/lib/libadpcm.so /lib/libbluetoothdrv.so /lib/libbluetooth_mtk.so \
+/lib/libbtcust.so /lib/libbtcusttable.so /lib/libbtcusttc1.so \
+/lib/libbtsession.so /lib/libbtstd.so \
+/lib/libextsys.so /lib/libextsys_jni.so \
+/lib/libpalsecurity.so /lib/libpalwlan_mtk.so \
+/lib/libbluetoothem_mtk.so /lib/libbluetooth_relayer.so \
+/lib/libbtem.so /lib/libbtpcm.so /lib/libbtsniff.so
+"
+
+SYSTEM="$FIRMWARE $WIFI $GL $DRM $RIL $AUDIO $BLUETOOTH"
 
 move_files () {
 	mv $TARGET/lib/hw/audio.primary.mt6595.so $TARGET/lib/libaudio.primary.default.so
