0408ee349f08958174a1d1d20c5c36dd885b3480
Wang Xuerui <idontknw.wang@gmail.com>
2015-08-25 02:57:09 +0800

    silence warnings from nonexistent directories

--- arale-vendor.mk
+++ arale-vendor.mk
@@ -4,8 +4,8 @@ PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/meizu/arale/bin,sys
 
 PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/meizu/arale/etc,system/etc)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/meizu/arale/app,system/app)
+# PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/meizu/arale/app,system/app)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/meizu/arale/xbin,system/xbin)
+# PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/meizu/arale/xbin,system/xbin)
 
 PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/meizu/arale/vendor,system/vendor)
